d053a6fc359f74da6832222a6a8deea424eb3a96
vimt <viavinmt@gmail.com>
2026-05-29 15:29:26 +0300

    Add correct Makefile and build.sh for Render

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-# скачиваем uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
+export PATH="$HOME/.local/bin:$PATH"
 
-# здесь добавьте все необходимые команды для установки вашего проекта
-# команду установки зависимостей, сборки статики, применения миграций и другие
 make install && make collectstatic && make migrate
