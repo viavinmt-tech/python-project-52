c928856a01d443c6e492c2874809b4c4d6535fa9
vimt <viavinmt@gmail.com>
2026-06-15 15:08:31 +0300

    Unify delete views: check in post method for all entities

--- task_manager/views.py
+++ task_manager/views.py
@@ -91,22 +91,6 @@ class TaskUpdateView(LoginRequiredMixin, UpdateView):
         messages.success(self.request, 'Задача успешно изменена')
         return response
 
-class TaskDeleteView(LoginRequiredMixin, DeleteView):
-    model = Task
-    template_name = 'task_delete.html'
-    success_url = reverse_lazy('tasks')
-    
-    def dispatch(self, request, *args, **kwargs):
-        task = self.get_object()
-        if task.author != request.user:
-            messages.error(request, 'Задачу может удалить только ее автор')
-            return redirect('tasks')
-        return super().dispatch(request, *args, **kwargs)
-    
-    def post(self, request, *args, **kwargs):
-        messages.success(request, 'Задача успешно удалена')
-        return super().post(request, *args, **kwargs)
-
 class TaskDetailView(LoginRequiredMixin, DetailView):
     model = Task
     template_name = 'task_detail.html'
@@ -138,22 +122,6 @@ class LabelUpdateView(LoginRequiredMixin, UpdateView):
         response = super().form_valid(form)
         messages.success(self.request, 'Метка успешно изменена')
         return response
-
-class LabelDeleteView(LoginRequiredMixin, DeleteView):
-    model = Label
-    template_name = 'label_delete.html'
-    success_url = reverse_lazy('labels')
-    
-    def dispatch(self, request, *args, **kwargs):
-        label = self.get_object()
-        if label.task_set.exists():
-            messages.error(request, 'Невозможно удалить метку, потому что она используется')
-            return redirect('labels')
-        return super().dispatch(request, *args, **kwargs)
-    
-    def post(self, request, *args, **kwargs):
-        messages.success(request, 'Метка успешно удалена')
-        return super().post(request, *args, **kwargs)
 
 class UserUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = User
@@ -228,6 +196,7 @@ class UserDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
 
 
+
 class StatusDeleteView(LoginRequiredMixin, DeleteView):
     model = Status
     template_name = 'status_delete.html'
@@ -252,4 +221,17 @@ class LabelDeleteView(LoginRequiredMixin, DeleteView):
             messages.error(request, 'Невозможно удалить метку, потому что она используется')
             return redirect('labels')
         messages.success(request, 'Метка успешно удалена')
+        return super().post(request, *args, **kwargs)
+
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    model = Task
+    template_name = 'task_delete.html'
+    success_url = reverse_lazy('tasks')
+    
+    def post(self, request, *args, **kwargs):
+        task = self.get_object()
+        if task.author != request.user:
+            messages.error(request, 'Задачу может удалить только ее автор')
+            return redirect('tasks')
+        messages.success(request, 'Задача успешно удалена')
         return super().post(request, *args, **kwargs)
