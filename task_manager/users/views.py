858dc098adabf8ba16ea702fb8650aa2d8246e49
vimt <viavinmt@gmail.com>
2026-06-30 15:45:35 +0300

    Fix: remove unused import CreateView

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView, LogoutView
