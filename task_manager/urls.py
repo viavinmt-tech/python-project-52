36f573773758d527b1adc71fc8a6cf5e0a7dcfdd
vimt <viavinmt@gmail.com>
2026-06-30 15:21:20 +0300

    Fix: add redirect

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from task_manager.views import home, trigger_error
 
 urlpatterns = [
     path('', home, name='home'),
+    path('login/', RedirectView.as_view(url='/users/login/', permanent=False), name='login_redirect'),
+    path('logout/', RedirectView.as_view(url='/users/logout/', permanent=False), name='logout_redirect'),
     path('users/', include('task_manager.users.urls')),
     path('statuses/', include('task_manager.statuses.urls')),
     path('tasks/', include('task_manager.tasks.urls')),
