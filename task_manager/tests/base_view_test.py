3c35e09ca7ea41779983f7004ec86b55ce80bd45
vimt <viavinmt@gmail.com>
2026-07-01 11:44:57 +0300

    Fix

--- task_manager/tests/base_view_test.py
+++ task_manager/tests/base_view_test.py
@@ -2,11 +2,17 @@ from django.urls import reverse
 from task_manager.tests.base import BaseTestCase
 
 class BaseViewTestCase(BaseTestCase):
+    url_name = None
+    
     def test_list_requires_login(self):
+        if self.url_name is None:
+            return
         self.client.logout()
         response = self.client.get(reverse(self.url_name))
         self.assertEqual(response.status_code, 302)
     
     def test_list_with_login(self):
+        if self.url_name is None:
+            return
         response = self.client.get(reverse(self.url_name))
         self.assertEqual(response.status_code, 200)
